5189adbbb30543bfd7182b2d766cce80a458a7b8
Tom King <design.tomking@gmail.com>
2018-11-05 11:42:12 -0700

    Create api call to retrieve dog images

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ info }}
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'app',
+  data () {
+    return {
+      info: null
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted () {
+    let path = "https://api.harvardartmuseums.org/object/";
+    let query = { params: {
+        apikey: "f8411350-e114-11e8-9234-6f5a1db33697",
+        title: "dog",
+        hasimage: 1,
+        color: 'any',
+        fields: "objectid,title,dated",
+    }}
+    axios.get(path, query).then(res => { 
+      res.data.records.forEach(r => {
+        let nextPath = path+r.objectid;
+        let options = {
+          params: {
+            apikey: "f8411350-e114-11e8-9234-6f5a1db33697",
+          }
+        }
+        axios.get(nextPath, options).then(res => {
+          console.log(res)
+          this.info = res
+        })
+      })
+    })
   }
 }
 </script>
@@ -23,6 +53,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
 }
 </style>
